03605202609617452d7519e004a07346ad6214af
connandgo <connandgo@gmail.com>
2025-07-05 03:05:45 +0900

    main setting

---
+++ src/main/java/com/mysite/aimusicsheet/AiMusicSheetApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.aimusicsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMusicSheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMusicSheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysite/aimusicsheet/AiMusicSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.aimusicsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMusicSheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
